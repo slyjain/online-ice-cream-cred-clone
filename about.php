b2d02347da6c0a90f2eb0edf09fdd397aaee589a
wolfstrix360 <144126612+wolfstrix360@users.noreply.github.com>
2023-11-20 10:05:20 +0530

    Update about.php

--- about.php
+++ about.php
@@ -8,6 +8,47 @@
     <header>
         <h1>Welcome to Frosty Delights</h1>
     </header>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us - ice cream</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Frosty Delights</h1>
+    </header>
+
+    <section class="section-4">
+        <div class="section-4-left">
+            <div class="section-4-left-content section-4-left-1">
+                <div class="section-4-left-heading" style="color:#F0BF4C">Our Story</div>
+                <div class="section-4-left-desc" style="color:#5BD1D7;">Our journey began in a small kitchen, where our founder, Samyak, experimented with different flavors and ingredients to create unique and mouthwatering ice cream recipes. Fueled by his passion for frozen treats and a desire to share his creations with the world, Samyak transformed his hobby into a full-fledged ice cream business.</div>
+            </div>
+            <div class="section-4-left-content section-4-left-2">
+                <div class="section-4-left-heading" style="color:#F0BF4C">Craftsmanship and Creativity</div>
+                <div class="section-4-left-desc" style="color:#5BD1D7;">At Sweet Scoops, we take pride in our craftsmanship and creativity. Each batch of ice cream is carefully handcrafted in small quantities, allowing us to maintain the highest standards of flavor and texture. Our talented team of ice cream artisans is always exploring new ideas and pushing the boundaries of taste to surprise and delight our customers.</div>
+            </div>
+            <!-- Other content -->
+        </div>
+
+        <!-- Div for images -->
+        <div class="image-gallery">
+            <img src="image1.jpg" alt="Image 1">
+            <img src="image2.jpg" alt="Image 2">
+            <!-- Add more images as needed -->
+        </div>
+    </section>
+
+    <footer style="color: #9EEB47; font-size:20px; font-weight:700;">
+        <p style="color: #9EEB47;">Come and visit us!</p>
+        <p style="color: #9EEB47;">Contact: info@FrostyDelights.com | Phone: 123-456-7890</p>
+    </footer>
+
+    <script src="jss/aboutscript.js"></script>
+</body>
+</html>
+
 
   
 
